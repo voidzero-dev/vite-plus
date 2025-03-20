997481851563cb82dd80ee03c80b4168b89033fb
branchseer <dk4rest@gmail.com>
2025-03-21 00:13:10 +0800

    fix: remove package.json in package path

--- crates/vite_workspace/src/main.rs
+++ crates/vite_workspace/src/main.rs
@@ -123,7 +123,7 @@ struct PackageGraphBuilder {
 impl PackageGraphBuilder {
     fn add_package(
         &mut self,
-        path: CompactString,
+        package_path: CompactString,
         package_json: PackageJson,
     ) -> anyhow::Result<()> {
         let deps = package_json
@@ -131,7 +131,7 @@ impl PackageGraphBuilder {
             .collect::<Vec<_>>();
         let id = self.graph.add_node(PackageInfo {
             name: package_json.name.clone(),
-            path,
+            path: package_path,
         });
         if let Some((existing_id, _)) = self
             .id_and_deps_by_name
@@ -164,29 +164,32 @@ fn get_package_graph(workspace_root: impl AsRef<Path>) -> anyhow::Result<Graph<P
     let workspace: PnpmWorkspace = serde_yml::from_str(&workspace_yaml)?;
     let member_globs = workspace.into_member_globs();
     let mut graph_builder = PackageGraphBuilder::default();
-    for member in member_globs.get_package_json_paths(workspace_root)? {
-        let package_json: PackageJson = serde_json::from_slice(&fs::read(&member)?)?;
-        let path = member.strip_prefix(workspace_root).with_context(|| {
+    for package_json_path in member_globs.get_package_json_paths(workspace_root)? {
+        let package_json: PackageJson = serde_json::from_slice(&fs::read(&package_json_path)?)?;
+        let package_path = package_json_path.parent().unwrap();
+        let package_path = package_path.strip_prefix(workspace_root).with_context(|| {
             format!(
                 "Package {} is outside the workspace {}",
-                member.display(),
+                package_path.display(),
                 workspace_root.display()
             )
         })?;
-        let path = path
+        let package_path = package_path
             .to_str()
-            .with_context(|| format!("Package path {:?} is not valid UTF-8", path))?;
-        graph_builder.add_package(path.into(), package_json)?;
+            .with_context(|| format!("Package path {:?} is not valid UTF-8", package_path))?;
+        graph_builder.add_package(package_path.into(), package_json)?;
     }
     Ok(graph_builder.build())
 }
 
 fn main() -> anyhow::Result<()> {
-    let package_graph = get_package_graph(current_dir()?)?;
+    let workspace_root = std::env::args_os().nth(1).unwrap_or_else(|| current_dir().unwrap().into_os_string()); 
+    let package_graph = get_package_graph(workspace_root)?;
     let graph_json = serde_json::to_vec(&package_graph)?;
     let server = tiny_http::Server::http("0.0.0.0:0").map_err(|err| anyhow::anyhow!(err))?;
 
     let port = server.server_addr().to_ip().unwrap().port();
+    println!("{}", serde_json::to_string_pretty(&package_graph)?);
     let url = format!("http://localhost:{port}/");
     println!("{url}");
     if let Err(err) = webbrowser::open(&url) {
