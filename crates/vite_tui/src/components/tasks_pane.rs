2d77c6a8fdc88b8947b96a58cae57215ba069d57
Boshen <boshenc@gmail.com>
2025-06-15 16:37:35 +0800

    add pseudo_term

--- crates/vite_tui/src/components/tasks_pane.rs
+++ crates/vite_tui/src/components/tasks_pane.rs
@@ -1,5 +1,11 @@
 use color_eyre::Result;
-use ratatui::{Frame, layout::Rect, widgets::Block};
+use ratatui::{
+    Frame,
+    layout::Rect,
+    widgets::{Block, Widget},
+};
+use tui_term::widget::PseudoTerminal;
+use vt100_ctt::Parser;
 
 use super::Component;
 
@@ -13,8 +19,12 @@ impl TasksPane {
 
 impl Component for TasksPane {
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
-        let block = Block::new();
-        frame.render_widget(block, area);
+        let mut parser = Parser::new(24, 80, 0);
+        parser.process(b"foo");
+        let screen = parser.screen();
+        let block = Block::default().title("Terminal");
+        let pseudo_term = PseudoTerminal::new(screen).block(block);
+        pseudo_term.render(area, frame.buffer_mut());
         Ok(())
     }
 }
