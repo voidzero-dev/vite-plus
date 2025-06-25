78c1e3f7e6428145bf16e28d07145773a5804535
Lars Kappert <lars@webpro.nl>
2025-06-25 15:21:40 +0200

    Add minimal-js template

---
+++ packages/global/templates/minimal-js/src/index.js
@@ -0,0 +1,11 @@
+/**
+ * Greetings earthlings, we have not taken over your radiooooo 🎶
+ *
+ * @param {string} name
+ * @returns {string}
+ */
+export const greet = name => {
+  return `Hello, ${name}!`;
+};
+
+console.log(greet("World"));
