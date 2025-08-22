0387d2e5c7e3fb6d053d6da7ecb2ca26b7948764
fengmk2 <fengmk2@gmail.com>
2025-08-22 20:35:25 +0800

    FIX

--- crates/vite_task/src/main.rs
+++ crates/vite_task/src/main.rs
@@ -10,13 +10,14 @@ async fn main() -> Result<(), Error> {
     init_tracing();
 
     let args = Args::parse();
-    let result = vite_task::main(current_dir()?, args, None::<CliOptions>).await;
-    if let Err(err) = result {
-        tracing::error!("Error: {}", err);
-        match err {
-            Error::UserCancelled => std::process::exit(130), // Standard exit code for Ctrl+C
-            _ => return Err(err),
+    match vite_task::main(current_dir()?, args, None::<CliOptions>).await {
+        Ok(()) => Ok(()),
+        Err(err) => {
+            tracing::error!("Error: {}", err);
+            match err {
+                Error::UserCancelled => std::process::exit(130), // Standard exit code for Ctrl+C
+                _ => return Err(err),
+            }
         }
     }
-    Ok(())
 }
