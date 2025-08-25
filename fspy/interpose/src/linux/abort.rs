deb419375084647a6b6b1f8c15b2f59b1241b23c
branchseer <dk4rest@gmail.com>
2025-08-25 19:27:15 +0800

    merge fspy/crates with crates

--- fspy/interpose/src/linux/abort.rs
+++
@@ -1,6 +0,0 @@
-use std::{fmt::Display, fs::File, io::Write, mem::ManuallyDrop, os::fd::FromRawFd};
-
-pub fn abort_with(msg: impl Display) -> ! {
-    libc_print::libc_eprintln!("{}", msg);
-    unsafe { libc::abort() }
-}
--- fspy/interpose/src/unix/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod libc;
-pub mod interpose;
-pub mod real;
