b3143bd1589ac720f66b909cd0da0ee89d779477
branchseer <dk4rest@gmail.com>
2025-09-11 17:08:39 +0800

    dprint tests

--- packages/cli/tests/main.ts
+++ packages/cli/tests/main.ts
@@ -1,9 +1,9 @@
-import { tmpdir } from 'node:os';
-import { randomUUID } from 'node:crypto'
-import fs from 'node:fs'
-import cp from 'node:child_process';
-import path from 'node:path'
 import assert from 'node:assert';
+import cp from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import fs from 'node:fs';
+import { tmpdir } from 'node:os';
+import path from 'node:path';
 
 // Create a unique temporary directory for testing
 const tempTmpDir = `${tmpdir()}/vite-plus-test-${randomUUID()}`;
@@ -15,46 +15,43 @@ process.on('exit', () => fs.rmSync(tempTmpDir, { recursive: true, force: true })
 const casesDir = import.meta.dirname + '/cases';
 
 for (const caseName of fs.readdirSync(casesDir)) {
-    if (caseName.startsWith('.')) continue; // Skip hidden files like .DS_Store
-    runTestCase(caseName);
+  if (caseName.startsWith('.')) continue; // Skip hidden files like .DS_Store
+  runTestCase(caseName);
 }
 
 interface Steps {
-    env: Record<string, string>;
-    commands: string[];
+  env: Record<string, string>;
+  commands: string[];
 }
 
 function runTestCase(name: string) {
-    const caseTmpDir = `${tempTmpDir}/${name}`;
-    fs.cpSync(`${casesDir}/${name}`, caseTmpDir, { recursive: true, errorOnExist: true });
+  const caseTmpDir = `${tempTmpDir}/${name}`;
+  fs.cpSync(`${casesDir}/${name}`, caseTmpDir, { recursive: true, errorOnExist: true });
 
+  const steps: Steps = JSON.parse(fs.readFileSync(`${caseTmpDir}/steps.json`, 'utf-8'));
 
-    const steps: Steps = JSON.parse(fs.readFileSync(`${caseTmpDir}/steps.json`, 'utf-8'));
+  const env = {
+    ...process.env,
+    ...steps.env,
+    // Indicate CLI is running in test mode
+    VITE_PLUS_CLI_TEST: '1',
+  };
+  env['PATH'] = [
+    ...env['PATH']!.split(path.delimiter),
+    // Extend PATH to include the CLI's bin directory
+    path.dirname(import.meta.dirname) + '/bin',
+    // Also include node_modules/.bin for local dev dependencies
+    path.dirname(import.meta.dirname) + '/node_modules/.bin',
+  ].join(path.delimiter);
 
-    const env = {
-        ...process.env,
-        ...steps.env,
-        // Indicate CLI is running in test mode
-        VITE_PLUS_CLI_TEST: '1',
-    };
-    env['PATH'] = [
-        ...env['PATH']!.split(path.delimiter),
-        // Extend PATH to include the CLI's bin directory
-        path.dirname(import.meta.dirname) + '/bin',
-        // Also include node_modules/.bin for local dev dependencies
-        path.dirname(import.meta.dirname) + '/node_modules/.bin',
-    ].join(path.delimiter);
+  const newSnap: string[] = [];
 
+  for (const command of steps.commands) {
+    newSnap.push(`> ${command}`);
+    const output = cp.execSync(command, { env, cwd: caseTmpDir });
+    newSnap.push(output.toString('utf8'));
+  }
+  const newSnapContent = newSnap.join('\n');
 
-
-    const newSnap: string[] = [];
-
-    for (const command of steps.commands) {
-        newSnap.push(`> ${command}`);
-        const output = cp.execSync(command, { env, cwd: caseTmpDir });
-        newSnap.push(output.toString('utf8'));
-    }
-    const newSnapContent = newSnap.join('\n');
-
-    fs.writeFileSync(`${casesDir}/${name}/snap.txt`, newSnapContent);
+  fs.writeFileSync(`${casesDir}/${name}/snap.txt`, newSnapContent);
 }
