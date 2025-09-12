eea62ed123753be8a62c9ee292a68d66b2200855
branchseer <dk4rest@gmail.com>
2025-09-12 17:28:44 +0800

    add json-edit internal tool

---
+++ packages/tools/README.md
@@ -0,0 +1,3 @@
+# tools for internal development use
+
+- json-edit: A CLI tool to edit JSON files such as package.json in e2e tests
---
+++ packages/tools/src/json-edit.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { readFileSync, writeFileSync } from 'node:fs'
+
+const filename = process.argv[2];
+const script = process.argv[3];
+
+if (!filename || !script) {
+    console.error('Usage: json-edit <filename> <script>');
+    console.error('Example: json-edit package.json \'$.version = "1.2.3"\'');
+    process.exit(1);
+}
+
+const json = JSON.parse(readFileSync(filename, 'utf-8'));
+const func = new Function('_', script + "; return _;");
+const result = func(json);
+
+writeFileSync(filename, JSON.stringify(result, null, 2) + '\n', 'utf-8');
