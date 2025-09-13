f64dec2ce5432cd732995441f43d0180ee4bab13
MK <fengmk2@gmail.com>
2025-09-13 19:40:11 +0800

    chore: make snapshot testing run in parallel

--- packages/tools/src/snap-test.ts
+++ packages/tools/src/snap-test.ts
@@ -3,8 +3,12 @@
 import cp from 'node:child_process';
 import { randomUUID } from 'node:crypto';
 import fs from 'node:fs';
+import fsPromises from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import path from 'node:path';
+import { promisify } from 'node:util';
+
+const exec = promisify(cp.exec);
 
 // Create a unique temporary directory for testing
 const tempTmpDir = `${tmpdir()}/vite-plus-test-${randomUUID()}`;
@@ -17,24 +21,27 @@ const casesDir = path.resolve('snap-tests');
 
 const filter = process.argv[2] ?? ''; // Optional filter to run specific test cases
 
+const tasks: Promise<void>[] = [];
 for (const caseName of fs.readdirSync(casesDir)) {
   if (caseName.startsWith('.')) continue; // Skip hidden files like .DS_Store
   if (caseName.includes(filter)) {
-    console.log(caseName);
-    runTestCase(caseName);
+    tasks.push(runTestCase(caseName));
   }
 }
+
+await Promise.all(tasks);
 
 interface Steps {
   env: Record<string, string>;
   commands: string[];
 }
 
-function runTestCase(name: string) {
+async function runTestCase(name: string) {
+  console.log('%s started', name);
   const caseTmpDir = `${tempTmpDir}/${name}`;
-  fs.cpSync(`${casesDir}/${name}`, caseTmpDir, { recursive: true, errorOnExist: true });
+  await fsPromises.cp(`${casesDir}/${name}`, caseTmpDir, { recursive: true, errorOnExist: true });
 
-  const steps: Steps = JSON.parse(fs.readFileSync(`${caseTmpDir}/steps.json`, 'utf-8'));
+  const steps: Steps = JSON.parse(await fsPromises.readFile(`${caseTmpDir}/steps.json`, 'utf-8'));
 
   const env = {
     ...process.env,
@@ -52,18 +59,29 @@ function runTestCase(name: string) {
 
   for (const command of steps.commands) {
     try {
-      const output = cp.execSync(command, { env, cwd: caseTmpDir, encoding: 'utf-8' });
+      const { stdout, stderr } = await exec(command, { env, cwd: caseTmpDir, encoding: 'utf-8' });
       newSnap.push(`> ${command}`);
-      newSnap.push(replaceUnstableOutput(output));
+      if (stdout) {
+        newSnap.push(replaceUnstableOutput(stdout));
+      }
+      if (stderr) {
+        newSnap.push(replaceUnstableOutput(stderr));
+      }
     } catch (error) {
-      // add error status code to the command
-      newSnap.push(`[${error.status}]> ${command}`);
-      newSnap.push(error.output.filter((line: string | null) => line !== null).join('\n'));
+      // add error exit code to the command
+      newSnap.push(`[${error.code}]> ${command}`);
+      if (error.stdout) {
+        newSnap.push(replaceUnstableOutput(error.stdout));
+      }
+      if (error.stderr) {
+        newSnap.push(replaceUnstableOutput(error.stderr));
+      }
     }
   }
   const newSnapContent = newSnap.join('\n');
 
-  fs.writeFileSync(`${casesDir}/${name}/snap.txt`, newSnapContent);
+  await fsPromises.writeFile(`${casesDir}/${name}/snap.txt`, newSnapContent);
+  console.log('%s finished', name);
 }
 
 function replaceUnstableOutput(output: string) {
