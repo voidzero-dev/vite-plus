48ca1bdf804ecd41f3de233e2d855e3c390fe558
MK <fengmk2@gmail.com>
2025-09-16 11:20:35 +0800

    feat(tools): ignore pnpm install unstable ouutput
    and ignore cwd too

---
+++ packages/tools/src/utils.ts
@@ -0,0 +1,24 @@
+export function replaceUnstableOutput(output: string, cwd?: string) {
+  if (cwd) {
+    output = output.replaceAll(cwd, '<cwd>');
+  }
+  return output
+    // semver version
+    .replaceAll(/ (v)?\d+\.\d+\.\d+(?:-.*)?/g, ' $1<semver>')
+    // oxlint
+    .replaceAll(/\d+(?:\.\d+)?s|\d+ms/g, '<variable>ms')
+    .replaceAll(/with \d+ rules using \d+ threads/g, 'with <variable> rules using <variable> threads')
+    // pnpm
+    .replaceAll(/Packages: \+\d+/g, 'Packages: +<variable>')
+    // only keep done
+    .replaceAll(
+      /Progress: resolved \d+, reused \d+, downloaded \d+, added \d+, done/g,
+      'Progress: resolved <variable>, reused <variable>, downloaded <variable>, added <variable>, done',
+    )
+    // ignore pnpm progress
+    .replaceAll(/Progress: resolved \d+, reused \d+, downloaded \d+, added \d+\n/g, '')
+    // ignore pnpm warn
+    .replaceAll(/WARN\s+Skip\s+adding .+?\n/g, '')
+    .replaceAll(/Scope: all \d+ workspace projects/g, 'Scope: all <variable> workspace projects')
+    .replaceAll(/\++\n/g, '+<repeat>\n');
+}
