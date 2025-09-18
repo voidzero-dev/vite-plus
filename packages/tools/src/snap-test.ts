df80692bc9b6dbfe24c3dd8581e5298717e83b7d
MK <fengmk2@gmail.com>
2025-09-18 09:38:39 +0800

    test: try to repeat panicked

--- packages/tools/src/snap-test.ts
+++ packages/tools/src/snap-test.ts
@@ -9,10 +9,11 @@ import path from 'node:path';
 import { promisify } from 'node:util';
 
 const cpExec = promisify(cp.exec);
-const exec = async (command: string, options: cp.ExecOptionsWithStringEncoding) => cpExec(
-  command,
-  process.platform === 'win32' ? { ...options, shell: 'pwsh.exe' } : options
-);
+const exec = async (command: string, options: cp.ExecOptionsWithStringEncoding) =>
+  cpExec(
+    command,
+    process.platform === 'win32' ? { ...options, shell: 'pwsh.exe' } : options,
+  );
 
 import { replaceUnstableOutput } from './utils.ts';
 
@@ -60,9 +61,9 @@ async function runTestCase(name: string) {
   };
 
   // Sometimes on Windows, the PATH variable is named 'Path'
-  if ("Path" in env && !("PATH" in env)) {
-    env['PATH'] = env["Path"];
-    delete env["Path"];
+  if ('Path' in env && !('PATH' in env)) {
+    env['PATH'] = env['Path'];
+    delete env['Path'];
   }
   env['PATH'] = [
     // Extend PATH to include the package's bin directory
