a460bf5f29f949676cb743291cb34dec120a0a6a
branchseer <dk4rest@gmail.com>
2025-10-23 10:30:14 +0800

    chore(fspy): remove ignoring Chromium processes

--- crates/fspy_shared_unix/src/spawn/macos.rs
+++ crates/fspy_shared_unix/src/spawn/macos.rs
@@ -5,7 +5,6 @@ use std::{
     path::{Path, absolute},
 };
 
-use bstr::ByteSlice;
 use phf::{Set, phf_set};
 
 use crate::{
@@ -42,14 +41,7 @@ pub fn handle_exec(
     let injectable = if let (Some(parent), Some(file_name)) =
         (program_path.parent(), program_path.file_name())
     {
-        // Exclude Chrome and Chromium-based browsers
-        if file_name.as_bytes().windows(6).any(|w| w == b"Chrome" || w == b"chrome")
-            || file_name.as_bytes().windows(8).any(|w| w == b"Chromium" || w == b"chromium")
-            || command.program.contains_str("Google Chrome")
-            || command.program.contains_str("Chromium")
-        {
-            false
-        } else if matches!(parent.as_os_str().as_bytes(), b"/bin" | b"/usr/bin") {
+        if matches!(parent.as_os_str().as_bytes(), b"/bin" | b"/usr/bin") {
             let fixtures = &encoded_payload.payload.fixtures;
             if matches!(file_name.as_bytes(), b"sh" | b"bash") {
                 command.program = fixtures.bash_path.as_os_str().as_bytes().into();
