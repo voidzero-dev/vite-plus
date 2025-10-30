bf57b00156916b77111798dfc1e004a636b761d2
LongYinan <lynweklm@gmail.com>
2025-10-30 14:52:57 +0800

    Lazy import

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,17 +1,16 @@
-import { replaceFileContent } from './replace-file-content.js';
-import { snapTest } from './snap-test.js';
-import { syncRemote } from './sync-remote-deps.js';
-
 const subcommand = process.argv[2];
 
 switch (subcommand) {
   case 'snap-test':
+    const { snapTest } = await import('./snap-test');
     await snapTest();
     break;
   case 'replace-file-content':
+    const { replaceFileContent } = await import('./replace-file-content');
     replaceFileContent();
     break;
   case 'sync-remote':
+    const { syncRemote } = await import('./sync-remote-deps');
     syncRemote();
     break;
   default:
