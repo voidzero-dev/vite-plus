3091f7d25236f3f4024427eaf7cf76e9bcb0ca34
LongYinan <lynweklm@gmail.com>
2026-01-07 15:07:40 +0800

    fix(test): deps and sideEffects

--- packages/cli/src/lib.ts
+++ packages/cli/src/lib.ts
@@ -1,2 +1,2 @@
-export { defineConfig, build, buildSingle, globalLogger } from '@voidzero-dev/vite-plus-core/lib';
+export { defineConfig, build, globalLogger } from '@voidzero-dev/vite-plus-core/lib';
 export type * from '@voidzero-dev/vite-plus-core/lib';
