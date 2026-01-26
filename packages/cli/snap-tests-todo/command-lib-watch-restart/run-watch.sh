08412f6fed8c97510d383414d23084ac5ad775a2
MK <fengmk2@gmail.com>
2026-01-26 13:29:37 +0800

    FIXUP

--- packages/cli/snap-tests-todo/command-lib-watch-restart/run-watch.sh
+++ packages/cli/snap-tests-todo/command-lib-watch-restart/run-watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start vite lib watch in background, redirect output to file, and save PID
-vite lib --debug --watch > .watch-output.log 2>&1 &
+vite lib --watch > .watch-output.log 2>&1 &
 echo $! > .pid
