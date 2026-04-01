b2831bb7f24058412e3c73bd819942c8e73fd42e
Dom Porada <dom@dom.engineering>
2026-04-01 16:32:48 +0200

    Include supported config types from `lint-staged`

--- packages/cli/src/staged-config.ts
+++ packages/cli/src/staged-config.ts
@@ -1,1 +1,13 @@
-export type StagedConfig = Record<string, string | string[]>;
+type StagedGenerateTask = (
+  stagedFileNames: readonly string[],
+) => string | string[] | Promise<string | string[]>;
+
+type StagedTaskFunction = {
+  title: string;
+  task: (stagedFileNames: readonly string[]) => void | Promise<void>;
+};
+
+export type StagedConfig = Record<
+  string,
+  string | string[] | StagedGenerateTask | StagedGenerateTask[] | StagedTaskFunction
+>;
