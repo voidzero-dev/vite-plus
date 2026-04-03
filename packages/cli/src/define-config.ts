71d3659ef6ae91e20efaa510eb2d8a94d516d4db
MK <fengmk2@gmail.com>
2026-04-03 15:25:07 +0800

    fix: add curly braces to satisfy eslint curly rule

--- packages/cli/src/define-config.ts
+++ packages/cli/src/define-config.ts
@@ -49,6 +49,8 @@ export function defineConfig(config: ViteUserConfigExport): ViteUserConfigExport
 
 export function vitePlugins<T>(cb: () => T): T | undefined {
   const cmd = process.env.VP_COMMAND;
-  if (cmd === 'dev' || cmd === 'build' || cmd === 'test' || cmd === 'preview') return cb();
+  if (cmd === 'dev' || cmd === 'build' || cmd === 'test' || cmd === 'preview') {
+    return cb();
+  }
   return undefined;
 }
