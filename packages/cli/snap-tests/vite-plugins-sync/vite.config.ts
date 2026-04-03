df1e199a680fd758c9a83de78f18a6aae6e5b9ff
MK <fengmk2@gmail.com>
2026-04-03 15:35:02 +0800

    test: add snap test for sync vitePlugins() usage

---
+++ packages/cli/snap-tests/vite-plugins-sync/vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig, vitePlugins } from 'vite-plus';
+import mySyncPlugin from './my-plugin';
+
+export default defineConfig({
+  plugins: vitePlugins(() => [mySyncPlugin()]),
+});
