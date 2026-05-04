ed939b2f080069ecab29c323a9c5a48f86f9238c
nozomee <nozomee@users.noreply.github.com>
2026-05-04 11:34:52 +0530

    docs: refresh home stats for oxlint, vite, and vitest

--- docs/.vitepress/theme/components/home/ProductivityGrid.vue
+++ docs/.vitepress/theme/components/home/ProductivityGrid.vue
@@ -31,13 +31,13 @@ import tileVitest from '@local-assets/tiles/vitest.png';
             <div
               class="relative flex flex-col flex-1 sm:pl-6 before:content-none sm:before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">69m+</p>
+              <p class="text-primary font-medium text-base sm:text-xl">115m+</p>
               <p class="leading-tight text-base">Weekly npm downloads</p>
             </div>
             <div
               class="relative flex flex-col flex-shrink-0 pl-6 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">78.7k</p>
+              <p class="text-primary font-medium text-base sm:text-xl">80.4k</p>
               <p class="leading-tight text-base">GitHub stars</p>
             </div>
           </div>
@@ -54,13 +54,13 @@ import tileVitest from '@local-assets/tiles/vitest.png';
             <div
               class="relative flex flex-col flex-1 sm:pl-6 before:content-none sm:before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">35m+</p>
+              <p class="text-primary font-medium text-base sm:text-xl">57m+</p>
               <p class="leading-tight text-base">Weekly npm downloads</p>
             </div>
             <div
               class="relative flex flex-col flex-shrink-0 pl-6 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">16.1k</p>
+              <p class="text-primary font-medium text-base sm:text-xl">16.5k</p>
               <p class="leading-tight text-base">GitHub stars</p>
             </div>
           </div>
@@ -77,13 +77,13 @@ import tileVitest from '@local-assets/tiles/vitest.png';
             <div
               class="relative flex flex-col flex-1 sm:pl-6 before:content-none sm:before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">5m+</p>
+              <p class="text-primary font-medium text-base sm:text-xl">5.3m+</p>
               <p class="leading-tight text-base">Weekly npm downloads</p>
             </div>
             <div
               class="relative flex flex-col flex-shrink-0 pl-6 before:content-[''] before:absolute before:left-0 before:top-1/2 before:-translate-y-1/2 before:h-full before:w-px before:bg-stroke"
             >
-              <p class="text-primary font-medium text-base sm:text-xl">19.8k</p>
+              <p class="text-primary font-medium text-base sm:text-xl">21.0k</p>
               <p class="leading-tight text-base">GitHub stars</p>
             </div>
           </div>
