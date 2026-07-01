d410ab0cd92b4bd78d0348339dac3259c2eaa143
wan9chi <dk4rest@gmail.com>
2026-07-01 21:47:14 +0800

    build(docs): trim build cache inputs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,12 +3,7 @@ export default {
     tasks: {
       'build:site': {
         command: 'vitepress build',
-        input: [
-          { auto: true },
-          '!.vitepress/.temp/**',
-          '!.vitepress/dist/**',
-          '!node_modules/.vite-temp/**',
-        ],
+        input: [{ auto: true }, '!.vitepress/.temp/**', '!.vitepress/dist/**'],
         output: ['.vitepress/dist/**'],
       },
     },
