3edb0c8815da706dad5c9a045af6e8c72b50dbc3
wan9chi <dk4rest@gmail.com>
2026-07-01 18:26:23 +0800

    ci(docs): enable cached docs build task

---
+++ docs/vite.config.ts
@@ -0,0 +1,16 @@
+export default {
+  run: {
+    tasks: {
+      'build:site': {
+        command: 'vitepress build',
+        input: [
+          { auto: true },
+          '!.vitepress/.temp/**',
+          '!.vitepress/dist/**',
+          '!node_modules/.vite-temp/**',
+        ],
+        output: ['.vitepress/dist/**'],
+      },
+    },
+  },
+};
